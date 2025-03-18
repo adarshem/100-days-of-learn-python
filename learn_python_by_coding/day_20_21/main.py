59d12b29cc992bb2d5e28ea120c5da16c3522c9c
adarshem <adarshem@gmail.com>
2025-03-17 20:16:20 -0700

    day_20: snake game initial design

---
+++ learn_python_by_coding/day_20_21/main.py
@@ -0,0 +1,20 @@
+import time
+from turtle import Turtle, Screen
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+# Create the Snake object
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
