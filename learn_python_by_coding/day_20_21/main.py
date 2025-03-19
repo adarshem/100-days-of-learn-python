cb888f8bef96f61e300a06c64ad09bb6acbe565c
adarshem <adarshem@gmail.com>
2025-03-18 19:28:00 -0700

    day_20: completed snake navigation

--- learn_python_by_coding/day_20_21/main.py
+++ learn_python_by_coding/day_20_21/main.py
@@ -8,8 +8,18 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
+# new_segment = Turtle(shape="square")
+# new_segment.left(90)
+
 # Create the Snake object
 snake = Snake()
+
+# Listen for keypress
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
--- learn_python_by_coding/day_20_21/snake.py
+++ learn_python_by_coding/day_20_21/snake.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
+from typing import List
 
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
 class Snake:
     def __init__(self):
-        self.segments = []
+        self.segments: List[Turtle] = []
         self.create_snake()
 
     def create_snake(self):
@@ -21,3 +22,19 @@ class Snake:
             self.segments[seg_num].goto(self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor())
 
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
